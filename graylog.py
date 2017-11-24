393f46a07ddd33faeeec6823045b7f1c955e8bf9
Ray Leon <rayl7786@gmail.com>
2017-11-24 03:54:46 +0000

    Graylog Script

---
+++ graylog.py
@@ -0,0 +1,66 @@
+from subprocess import call
+from colorama import Fore, init
+init(autoreset=True)
+import wget
+import fileinput
+
+# Update repository
+print(Fore.GREEN + "\n*** Running apt update command ***\n")
+call(["apt", "update"])
+
+# Upgrade packages
+print(Fore.GREEN + "\n*** Running apt upgrade command ***\n")
+call(["apt", "upgrade", "-y"])
+
+# Install necessary packages for complete graylog setup
+print(Fore.GREEN + "\n*** Installing necessary packages ***\n")
+call(["apt", "-y", "install", "apt-transport-https", "openjdk-8-jre-headless", "uuid-runtime", "pwgen"])
+
+# Install Elasticsearch locally
+elasticsearch_answer = input("\nWould you like Elasticsearch installed locally? ").lower()
+if elasticsearch_answer.startswith('y'):
+  print(Fore.GREEN + "\n*** Installing Elasticsearch ***\n") 
+  call(["wget", "https://artifacts.elastic.co/GPG-KEY-elasticsearch"])
+  call(["apt-key", "add", "GPG-KEY-elasticsearch"]) 
+  call(["echo", "deb https://artifacts.elastic.co/packages/5.x/apt stable main", "|", "tee", "-a", "/etc/apt/sources.list.d/elastic-5.x.list"])
+  call(["apt", "update"])
+  call(["apt", "-y", "install", "elasticsearch"])
+  call(["update-rc.d", "elasticsearch", "defaults", "95", "10"])
+#else:
+
+# Install MongoDB locally
+mongodb_answer = input("\nWould you like MongoDB installed locally? ").lower()
+if mongodb_answer.startswith('y'):
+  print(Fore.GREEN + "\n*** Installing MongoDB ***\n")
+  call(["apt", "-y", "install", "mongodb-server"])
+#else:
+
+# Download graylog package & graylog script
+graylog_package_url = 'https://packages.graylog2.org/repo/packages/graylog-2.3-repository_latest.deb'
+graylog_script_url = '
+graylog_filename = wget.download(graylog_package_url, out="/tmp")
+print(Fore.GREEN + "\n*** Downloading and Installing Graylog package ***\n")
+graylog_filename
+print(Fore.GREEN + "\n*** Downloading Graylog Script ***\n")
+
+
+# Install graylog package
+call(["dpkg", "-i", "/tmp/graylog-2.3-repository_latest.deb"])
+
+# Update repository and install graylog
+call(["apt", "update"])
+call(["apt", "-y", "install", "graylog-server"])
+call(["systemctl", "daemon-reload"])
+call(["systemctl", "enable", "graylog-server.service"])
+
+# Configure graylog configuration file
+print(Fore.GREEN + "\n*** Configuring Graylog ***\n")
+
+# Run shell script to configure server.conf
+call(["sh", "/root/graylog_conf.sh"])
+
+# Start Graylog Server
+print(Fore.GREEN + "\n*** Starting Graylog ***\n")
+call(["systemctl", "start", "graylog-server.service"])
+
+
---
+++ graylog_conf.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+# Copy current graylog conf file to temp file
+file_path=$(mktemp -t graylog_XXX)
+cp /etc/graylog/server/server.conf $file_path
+
+# Generate Secret Password
+secret_password=$(pwgen -N 1 -s 96)
+
+# Create Username
+read -p 'Username for graylog WebGUI login? : ' username
+
+# Create Username password
+read -p "What will be the ${username} user password? : " password
+
+# Configure necessary parts of graylog configuration file
+sed -i "/^password_secret/c\password_secret = ${secret_password}" ${file_path}
+sed -i "/^#root_username/c\root_username = ${username}" ${file_path}
+
+# Convert password provided into hash password
+password=$(echo -n ${password} | shasum -a 256 | awk -F' ' '{print $1}')
+sed -i "/^root_password_sha2/c\root_password_sha2 = ${password}" ${file_path}
+
+# Set Elasticsearch IP
+get_elasticsearch_ip()
+{
+elasticsearch=$(dpkg -l | grep elasticsearch | wc -l)
+if [ ${elasticsearch} > 0 ]; then
+	read -p 'What will be the Elasticsearch host IP? : ' elasticsearch_ip
+	if [ -z ${elasticsearch_ip} ]; then
+		echo "\n***Nothing was entered***\n"
+		get_elasticsearch_ip
+	fi
+else
+	elasticsearch_ip=127.0.0.1
+	#ES_conf=$(/etc/elasticsearch/elasticsearch.yml)
+	#sed -i "/^network.host/c\network.host
+fi
+}
+
+get_elasticsearch_ip
+sed -i "/^#elasticsearch_hosts/c\elasticsearch_hosts = http://${elasticsearch_ip}:9200" ${file_path}
+
+# Set MongoDB IP
+get_mongodb_ip()
+{
+mongodb=$(dpkg -l | grep mongodb-server | wc -l)
+if [ ${mongodb} > 0 ]; then
+	read -p 'What will be the MongoDB host IP? : ' mongodb_ip
+	if [ -z ${mongodb_ip} ]; then
+		echo "\n***Nothing was entered***\n"
+		get_mongodb_ip
+	fi
+else
+	mongodb_ip=localhost
+fi
+}
+
+get_mongodb_ip
+sed -i "/^mongodb_uri/c\mongodb_uri = mongodb://${mongodb_ip}/graylog" ${file_path}
+
+# Set Graylog IP to local ip
+get_graylog_ip()
+{
+read -p 'What is the interface name to be used for graylog? : ' int_name
+if [ -z ${int_name} ]; then
+	echo "\n***Nothing was entered***\n"
+	get_graylog_ip
+fi
+}
+
+get_graylog_ip
+graylog_ip=$(/sbin/ifconfig ${int_name} | grep "inet " | awk -F' ' '{print $2}' | awk '{print $1}')
+sed -i "/^#http_publish_uri/c\http_publish_uri = http://${graylog_ip}:9000/" ${file_path}
+sed -i "/^#http_external_uri/c\http_external_uri = http://${graylog_ip}:9000/api" ${file_path}
+
+
